3ba57e67734c7bd26c530c161fc0a7ade55a660e
artur mf <163214303+arturmf1009@users.noreply.github.com>
2025-02-04 20:49:13 -0300

    Update Cardapio.java

--- Cardapio.java
+++ Cardapio.java
@@ -8,12 +8,12 @@ public enum Cardapio {
 
     //atributos
     private double price;
-    private int plateNumber;
+    private int plateCode;
 
     //construtor
     Cardapio(double price, int plateNumber) {
         this.price = price;
-        this.plateNumber = plateNumber;
+        this.plateCode = plateNumber;
     }
 
     //útil para quando um prato for pedido, enviar o preço dele para a conta, fazendo assim a soma do que foi pedido ao total da mesa
@@ -23,17 +23,18 @@ public enum Cardapio {
 
     //útil para quando um determinado prato for pedido, enviar o código dele para a cozinha para que esta manipule da forma que for necessário
     public int getPlateNumber() {
-        return plateNumber;
+        return plateCode;
     }
 
     //coloquei caso queira percorrer a lista de pratos
+    /*
     public static Cardapio getPlate(int plateNumber) {
         for (Cardapio plate : Cardapio.values()) {
-            if (plate.getPlateNumber == plateNumber) {
+            if (plate.getPlateNumber() == plateNumber) {
                 return plate;
             }
         }
         return null; // Prato não encontrado
     }
-}
+    */
 }
