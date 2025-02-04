c3b467b2427cb0cc946d74b975070dc94c71c7a7
artur mf <163214303+arturmf1009@users.noreply.github.com>
2025-02-04 19:33:53 -0300

    Update Funcionario.java
    base da classe baseado no que foi exposto paradigma visual

--- Funcionario.java
+++ Funcionario.java
@@ -1,5 +1,33 @@
-package F30Restaurantes;
+package trabalhotrinta;
 
-public abstract class Funcionario {
+public class Funcionarios {
+    protected String name;
+    protected double salary;
+    protected String turn;
+    private static int employersCounter = 0;
+
+    public Funcionarios(String name, double salary, String turn) {
+        this.name = name;
+        this.salary = salary;
+        this.turn = turn;
+        employersCounter++;
+    }
+
+    public static int getEmployersCounter() {
+        return employersCounter;
+    }
     
+    public void setSalary (double salary) {
+        this.salary = salary;
+    }
+    
+    public void setTurn (String turn) {
+        this.turn = turn;
+    }
+    
+    public void showInfo() {
+        System.out.println("Nome: " + name);
+        System.out.println("Salário: R$" + salary);
+        System.out.println("Turno: " + turn);
+    }
 }
