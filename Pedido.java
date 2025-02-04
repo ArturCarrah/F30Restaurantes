9844cafd916d7028b42dbcda12cae408686c371f
artur mf <163214303+arturmf1009@users.noreply.github.com>
2025-02-04 19:35:22 -0300

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,18 @@
+package trabalhotrinta;
+        
+public class Pedido {
+    private int orderNumber;
+    private Cardapio plate;
+    
+    //ver de automatizar esse input do pedido -> só fazer ele iterar automaticamente qnd for usar 
+    public Pedido(int orderNumber, Cardapio plate) {
+        this.orderNumber = orderNumber;
+        this.plate = plate;
+    }
+
+    public void exibirPedido() {
+        System.out.println("Pedido #" + orderNumber);
+        System.out.println("Prato: " + plate);
+        System.out.println("Preço: R$" + plate.getPrice());
+    }
+}
