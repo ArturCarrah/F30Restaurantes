1f1e409526f62a38c0cf7e50e32511806ac1fa88
artur mf <163214303+arturmf1009@users.noreply.github.com>
2025-02-08 11:32:32 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,26 +1,38 @@
 package trabalhotrinta;
 
 public abstract class Funcionario {
-    protected String name;
+    
+    
+    //once you create an employer he can't change his name -> fuck LGBT people
+    protected final String name;
     protected double salary;
     private static int employersCounter = 0;
 
-    //criar tratamento para fornecimento de parâmetros inválidos
+    
+    //create treatment for supplying invalid parameters
     public Funcionario (String name, double salary) {
         this.name = name;
         this.salary = salary;
         employersCounter++;
     }
 
+    
     public static int getEmployersCounter() {
         return employersCounter;
     }
+    
     
     public void setSalary (double salary) {
         this.salary = salary;
     }
 
-    //preferir a formatação toString
-    public abstract void showInfo();
-    //cada subclasse implementa por meio de um @Override 
+    //we use override to reuse the object class's toString method, instead of creating a showInfo() method
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nSalary: " + String.valueOf(salary);
+    }
+
+
+    //each subclass must define its own way of calculating salary based on the employer's role
+    public abstract double calculateSalary();
 }
