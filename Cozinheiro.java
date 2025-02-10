cae54ee65f15be0bd823e01379d81d9a2b896aa8
Gustagol10-eng <gustavvomt@gmail.com>
2025-02-10 18:41:44 -0300

    Create Cozinheiro.java
    criei a classe cozinheiro

---
+++ Cozinheiro.java
@@ -0,0 +1,41 @@
+package trabalhotrinta;
+
+public class Cozinheiro extends Funcionario{
+    protected int habilidade;
+    protected boolean preparandoPedido;
+    protected boolean chefe;
+
+    public void prepararPedido(Pedido pedido){
+        preparandoPedido = true;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return 0;
+    }
+
+    public Cozinheiro(String name, double salario, int habilidade, boolean preparandoPedido, boolean chefe){
+        super(name, salario);
+        this.habilidade = habilidade;
+        this.preparandoPedido = preparandoPedido;
+        this.chefe = chefe;
+    }
+    public int getHabilidade() {
+        return this.habilidade;
+    }
+    public void setHabilidade(int habilidade) {
+        this.habilidade = habilidade;
+    }
+    public boolean getPreparandoPedido() {
+        return this.preparandoPedido;
+    }
+    public void setPreparandoPedido(boolean preparandoPedido) {
+        this.preparandoPedido = preparandoPedido;
+    }
+    public boolean getChefe() {
+        return this.chefe;
+    }
+    public void setChefe(boolean chefe) {
+        this.chefe = chefe;
+    }
+}
