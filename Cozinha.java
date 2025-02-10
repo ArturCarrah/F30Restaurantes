ec2aa282ec7a29ce09006ba39138c891f68d26f7
pedro <pedroaurino1928@gmail.com>
2025-02-10 18:41:48 -0300

    Comentei

--- Cozinha.java
+++ Cozinha.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cozinha {
+    //PedidosSendoPrepados é um ArrayList de Pedido
     private List<Pedido> pedidosSendoPreparados;
+    //Cozinheiros é um ArrayList de Pedido
     private List<Cozinheiro> cozinheiros;
 
+    //Construtor
     public Cozinha() {
         this.pedidosSendoPreparados = new ArrayList<>();
         this.cozinheiros = new ArrayList<>();
     }
 
+    //Getters
     public List<Pedido> getPedidosSendoPreparados() {
         return pedidosSendoPreparados;
     }
@@ -19,4 +23,7 @@ public class Cozinha {
     public List<Cozinheiro> getCozinheiros() {
         return cozinheiros;
     }
+
+
+
 }
