baf2e99c5e96a3443cc3f3f5598f4cb054fd6c6c
J04z1n <joaovictorabreu@alu.ufc.br>
2025-02-13 08:43:04 -0300

    Coloquei umas funções a mais (FALTA COISA)]

--- atendimento/Mesa.java
+++ atendimento/Mesa.java
@@ -1,27 +1,50 @@
 package trabalhotrinta.atendimento;
 
-public class Mesa {
-    protected double totalGasto;
-    protected int numeroMesa = 0;
-    protected boolean estaOcupada;
+public class Mesa implements Pedido{
+    private double totalGasto;
+    private int numeroMesa;
+    private boolean isOcupada;
     
-    public Mesa (double totalGasto, boolean estaOcupada) {
-        numeroMesa++;
+    // TODO: implementar os métodos abstratos da interface Pedido
+
+    public Mesa(int numeroMesa, boolean isOcupada) {
         this.numeroMesa = numeroMesa;
-        this.totalGastot = totalGasto;
-        this.estaOcupada = estaOcupada;
+        this.isOcupada = isOcupada; 
+        this.totalGasto = 0;
     }
     
-    //
-    public void setPedido(Pedido pedido){
-        
+
+    public void listarCardapio(){
+        System.out.println("Peça ao garçom o prato pelo nome ou pelo código!");
+        System.out.println("\n========CARDÁPIO RESTAURANTE F30=========\n");
+        System.out.println("Código");
+        for(Cardapio prato : Cardapio.values()){
+            System.out.println(prato.getCodigo() + " ....... " + prato.name() + " " + prato.getPreco() + "R$");
+        }
     }
     
-    //retorna o quanto deve ser pago e "limpa" a mesa
-    public double realizarPagamento(double totaGasto) {
-        System.out.println("O total a ser pago é: " + totalGasto);
+
+    public void fazerPedido(Cardapio prato){
+        if(this.isOcupada == false){
+            this.isOcupada = true;
+            // Se o pedido foi feito, então alguém está ocupando a mesa
+        }
+        this.totalGasto = this.totalGasto + prato.getPreco();
+
+        // TODO: Implementar um jeito de passar o prato pro garçom
+        // O pedido tem que ir Mesa -> Garçom -> Cozinha
+
+    }
+
+
+    public int getNumeroMesa(){
+        return this.numeroMesa;
+    }
+    
+    // retorna o quanto deve ser pago e "limpa" a mesa
+    public void pagarConta(double totalGasto) {
+        System.out.println("O total pago pela mesa " + this.getNumeroMesa() + " foi:"  + totalGasto + "R$");
         totalGasto = 0;
-        estaOcupado = false;
-        return 0;
+        this.isOcupada = false;
     }
 }
