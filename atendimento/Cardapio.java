d3050127d0385f296d25371b81c171e8cefd939f
artur mf <163214303+arturmf1009@users.noreply.github.com>
2025-02-15 19:12:23 -0300

    Update Cardapio.java
    adaptei umas coisa para implementar pedido em garçom

--- atendimento/Cardapio.java
+++ atendimento/Cardapio.java
@@ -1,40 +1,50 @@
 package trabalhotrinta.atendimento;
 
 public enum Cardapio {
-    ARROZ(10.0, 1), 
-    BATATA_FRITA(15.0, 2), 
-    PIZZA(30.0, 3), 
-    COCA_COLA(5.0, 4);
+    ARROZ("Arroz", 10.0, 1),
+    BATATA_FRITA("Batata Frita", 15.0, 2), 
+    PIZZA("Pizza", 30.0, 3),
+    COCA_COLA("Coca-Cola", 5.0, 4);
 
-    //atributos
-    private double precoPrato;
-    private int pratoCodigo;
+    // Atributos
+    private final String pratoNome;
+    private final double precoPrato;
+    private final int pratoCodigo;
 
+    
     //construtor
-    Cardapio(double precoPrato, int pratoCodigo) {
+    Cardapio(String pratoNome, double precoPrato, int pratoCodigo) {
+        this.pratoNome = pratoNome;
         this.precoPrato = precoPrato;
         this.pratoCodigo = pratoCodigo;
     }
 
+    
     //útil para quando um prato for pedido, enviar o preço dele para a conta, fazendo assim a soma do que foi pedido ao total da mesa
-    public double getPrecoPrato() {
+    public double getPratoPreco() {
         return precoPrato;
     }
 
+    
     //útil para quando um determinado prato for pedido, enviar o código dele para a cozinha para que esta manipule da forma que for necessário
     public int getPratoCodigo() {
         return pratoCodigo;
     }
 
+    
+    public String getPratoNome() {
+        return pratoNome;
+    }
+    
+    
     //coloquei caso queira percorrer a lista de pratos
-    /*
-    public static Cardapio getPlate(int plateCode) {
-        for (Cardapio plate : Cardapio.values()) {
-            if (plate.getPratoCodigo() == pratoCodigo) {
-                return dish
+    public static Cardapio mostrarPrato(int pratoCodigo) {
+        for (Cardapio prato : Cardapio.values()) {
+            if (prato.getPratoCodigo() == pratoCodigo) {
+                System.out.println("Prato: " + prato.getPratoNome());
+                System.out.println("Preço: " + prato.getPratoPreco());
             }
         }
         return null; // Prato não encontrado
     }
-    */
 }
